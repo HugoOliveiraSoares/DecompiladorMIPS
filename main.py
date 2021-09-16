cee804ab07eba01eaed5f4921ffb7feb024efcf8
Hugo Oliveira Soares <hugoliveira.soares@gmail.com>
2021-09-16 17:55:47 -0300

    Convertendo instruções do tipo R

---
+++ main.py
@@ -0,0 +1,39 @@
+from Tabelas import Funct, Registradores
+
+
+def converterb_d(n):
+    decimal = 0
+    n = str(n)
+    n = n[::-1]
+    tam = len(n)
+    for i in range(tam):
+        if n[i] == "1":
+            decimal = decimal + 2**i
+    return decimal
+
+
+instrucao = "00010010111000000000000000010000"
+
+if instrucao.startswith("000000"):
+    rs = instrucao[6:11]
+    rt = instrucao[11:16]
+    rd = instrucao[16:21]
+    funct = instrucao[26:]
+    print(rs)
+    print(rt)
+    print(rd)
+    print(funct)
+    comando = '{} ${}, ${}, ${} '
+    print(comando.format(Funct(funct), Registradores(rd), Registradores(rs), Registradores(rt)))
+
+else:
+    opcode = instrucao[:6]
+    rs = instrucao[6:11]
+    rt = instrucao[11:16]
+    offset = converterb_d(instrucao[16:])
+    print(opcode)
+    print(rs)
+    print(rt)
+    print(offset)
+    comando = '{} ${}, {}(${}) '
+    print(comando.format(Funct(opcode), Registradores(rt), offset, Registradores(rs)))
